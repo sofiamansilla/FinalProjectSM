9084019ccfbdecbfeadc459ac5c3af10b6276f16
Sofia Mansilla <sofia.developer@proton.me>
2024-03-24 14:09:56 -0300

    En develop, funcionando ok llamado a APi desde home y rutas

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 // import Card from '../Components/Card'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 export const Home = () => {
+
+  const [dentists, setDentists] = useState([]) //Array que almacenara los dentistas obtenidos desde la API
+
+  useEffect(()=>{
+
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => res.json())
+    .then(res => setDentists(res))
+  }, [])
+  
+  console.log(dentists);
+  
   return (
     <main className="" >
       <h1>Home</h1>
@@ -13,4 +26,3 @@ export const Home = () => {
     </main>
   )
 }
-
