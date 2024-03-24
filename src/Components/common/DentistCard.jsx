c955709f2c7a140b4551b4ea3b1942660fc920c3
Sofia Mansilla <sofia.developer@proton.me>
2024-03-24 20:22:33 -0300

    checkpoint

--- src/Components/common/DentistCard.jsx
+++ src/Components/common/DentistCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link} from "react-router-dom"
 
 
 export const DentistCard = ({dentist}) => {
@@ -29,7 +29,7 @@ export const DentistCard = ({dentist}) => {
 
     <h2>{dentist.name}</h2>
     <h3>{dentist.username}</h3>
-    <Link to={`/Detail/{dentist.id}`}><button>Ver información del profesional</button></Link>
+    <Link to={`/Detail/${dentist.id}`}><button>Ver información del profesional</button></Link>
 
 
             {/* En cada card deberan mostrar en name - username y el id */}
