be34fded652bd37bd8d0673824975a999efb85ed
Sofia Mansilla <sofia.developer@proton.me>
2024-03-26 01:03:43 -0300

    arreglando validacion de nombre

--- src/services/validateName.js
+++ src/services/validateName.js
@@ -1,7 +1,7 @@
 export const validateFullName =(name)=> {
 
     const regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖ]{2,}$/;
-    return regex.test(name);
+    return regex.test(name.split(" ")[0]) && regex.test(name.split(" ")[1]);
 
   }
 
